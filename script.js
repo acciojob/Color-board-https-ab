ff545abb017c62eec171c7dcce84fadd44ca1a31
Anushka A. Bhasme <78693700+https-ab@users.noreply.github.com>
2025-09-25 10:08:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let board = document.getElementById("board");
+
+for (let i = 0; i < 800; i++) {
+	let box = document.createElement("div");
+	box.classList.add("box");
+	board.appendChild(box);
+}
