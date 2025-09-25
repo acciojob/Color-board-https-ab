2787a080efde595222ac43a7388006c1350a8de6
Anushka A. Bhasme <78693700+https-ab@users.noreply.github.com>
2025-09-25 10:52:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 //your JS code here. If required.
-let board = document.querySelector(".container");
+let container = document.getElementById('container');
 
 for (let i = 0; i < 800; i++) {
-	let square = document.createElement("div");
-	square.classList.add("square");
-	board.appendChild(square);
+    let box = document.createElement('div');
+    box.classList.add('square'); 
+    container.appendChild(box);
+
+    box.addEventListener('mouseover', () => {
+        let randomColor = getRandomColor();
+        box.style.backgroundColor = randomColor;
+    });
+
+    box.addEventListener('mouseout', () => {
+        setTimeout(() => {
+            box.style.backgroundColor = "gray";  
+        }, 1000);  
+    });
 }
+
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
