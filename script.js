f70389d27a21a73ee560eb98ba2a36cca05a2eda
Anushka A. Bhasme <78693700+https-ab@users.noreply.github.com>
2025-09-30 09:35:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,48 @@
-//your JS code here. If required.
-let container = document.getElementById('container');
+// //your JS code here. If required.
+// let container = document.getElementById('container');
 
+// for (let i = 0; i < 800; i++) {
+//     let box = document.createElement('div');
+//     box.classList.add('square'); 
+//     container.appendChild(box);
+
+//     box.addEventListener('mouseover', () => {
+//         let randomColor = getRandomColor();
+//         box.style.backgroundColor = randomColor;
+//     });
+
+//     box.addEventListener('mouseout', () => {
+//         setTimeout(() => {
+//             box.style.backgroundColor = "gray";  
+//         }, 1000);  
+//     });
+// }
+
+// function getRandomColor() {
+//     let letters = '0123456789ABCDEF';
+//     let color = '#';
+//     for (let i = 0; i < 6; i++) {
+//         color += letters[Math.floor(Math.random() * 16)];
+//     }
+//     return color;
+// }
+
+let container = document.querySelector('.container');
+let arr = ['1','2','3','4','5','6','7','8','9','0','a','b','c','d','e','f']
 for (let i = 0; i < 800; i++) {
-    let box = document.createElement('div');
-    box.classList.add('square'); 
-    container.appendChild(box);
-
-    box.addEventListener('mouseover', () => {
-        let randomColor = getRandomColor();
-        box.style.backgroundColor = randomColor;
-    });
-
-    box.addEventListener('mouseout', () => {
-        setTimeout(() => {
-            box.style.backgroundColor = "gray";  
-        }, 1000);  
-    });
+	let random = "#"
+	for(let i=0 ; i<6 ; i++){
+		random += arr[Math.floor(Math.random()*16)]
+	}
+	let square = document.createElement('div');
+	square.classList.add('square');
+	square.addEventListener('mouseover',()=>{
+		square.style.backgroundColor = random;
+		square.style.transition = "none";
+	})
+	square.addEventListener('mouseout',()=>{
+		square.style.backgroundColor = "rgb(29, 29, 29)";
+		square.style.transition = "background-color 1s ease";
+	})
+	container.appendChild(square);
 }
-
-function getRandomColor() {
-    let letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
-
